4fa43c87af6519476d4d71696cfba5ae82dabd61
JongYo-Park <kioasdy1@gmail.com>
2024-08-08 00:56:01 +0900

    몫 구하기

---
+++ CodingTest_몫 구하기/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CodingTest_몫_구하기
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = num1 / num2;
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int num1 = 4;
+            int num2 = 2;
+
+            Console.WriteLine(solution.solution(num1, num2));
+        }
+    }
+}
