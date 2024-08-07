0f1c412d65c8f2886a9ea43c96afea53ea388419
JongYo-Park <kioasdy1@gmail.com>
2024-08-08 01:22:35 +0900

    숫자 비교하기

---
+++ CodingTest_숫자 비교하기/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CodingTest_숫자_비교하기
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                if (num1 != num2)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int num1 = 3; 
+            int num2 = 4;
+            
+            Console.WriteLine(solution.solution(num1, num2));
+        }
+    }
+}
