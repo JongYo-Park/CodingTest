9033ab10aa15f5ad7a1bf36af34d41ee49cf2398
JongYo-Park <kioasdy1@gmail.com>
2024-08-08 00:53:34 +0900

    두수의 차

---
+++ CodingTest_두수의 차/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CodingTest_두수의_차
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = num1 - num2;
+                return answer;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int num1 = 3;
+            int num2 = 5;
+
+            Console.WriteLine(solution.solution(num1, num2));
+        }
+    }
+}
