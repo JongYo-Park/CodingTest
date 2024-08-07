4400be138cbc65dc4f54ee40775c811004a5a050
JongYo-Park <kioasdy1@gmail.com>
2024-08-07 18:16:41 +0900

    두수의 곱

---
+++ Coding Test_두수의 곱/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Coding_Test_두수의_곱
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                if (num1 == num2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int num1 = 2;
+            int num2 = 2;
+            
+            Console.WriteLine(solution.solution(num1, num2));
+        }
+    }
+}
